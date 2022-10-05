7dd034c919886618ce949e8539212fa634181412
Jinsujin <sylpid003@gmail.com>
2022-10-06 02:21:32 +0900

    feat: scene 띄우고 플레이어 노드 생성

--- Arcade/Arcade/AvoidDdong/AvoidDdongViewController.swift
+++ Arcade/Arcade/AvoidDdong/AvoidDdongViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import SpriteKit
 
 class AvoidDdongViewController: UIViewController {
 
+    private let skView = SKView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .orange
+        
+        view.addSubview(skView)
+        skView.frame.size = view.frame.size
+        
+        let scene = AvoidDdongScene(size: skView.frame.size)
+        scene.scaleMode = .resizeFill
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.presentScene(scene)
+    }
+}
+
+class AvoidDdongScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .white
+        
+        let player = SKSpriteNode(color: .red, size: CGSize(width: 40, height: 40))
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        addChild(player)
     }
 }
